8376cf8e0cb3329992ec0c1602e9746cf7f7cfa8
Vinicios Pires <contato.viniciospires@gmail.com>
2022-01-27 00:31:15 -0300

    Incluindo o verificado (selecionado)

--- script.js
+++ script.js
@@ -1,49 +1,65 @@
 // SELECIONANDO COMIDA 1
 function selecionarComida1() {
     document.getElementById("comida1").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectComida1").style.display = "block"
     
 // DESELECIONANDO COMIDA 2 3 4
     document.getElementById("comida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida2").style.display = "none"
 
     document.getElementById("comida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida3").style.display = "none"
     
     document.getElementById("comida4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida4").style.display = "none"
 }
 
 // SELECIONANDO COMIDA 2
 function selecionarComida2() {
     document.getElementById("comida2").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectComida2").style.display = "block"
     
 // DESELECIONANDO COMIDA 1 3 4
     document.getElementById("comida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida1").style.display = "none"
     
     document.getElementById("comida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida3").style.display = "none"
     
     document.getElementById("comida4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida4").style.display = "none"
 }
 
 // SELECIONANDO COMIDA 3
 function selecionarComida3() {
     document.getElementById("comida3").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectComida3").style.display = "block"
     
 // DESELECIONANDO COMIDA 1 2 4
     document.getElementById("comida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida1").style.display = "none"
     
     document.getElementById("comida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida2").style.display = "none"
     
     document.getElementById("comida4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida4").style.display = "none"
 }
 
 // SELECIONANDO COMIDA 4
 function selecionarComida4() {
     document.getElementById("comida4").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectComida4").style.display = "block"
     
 // DESELECIONANDO COMIDA 1 2 3
     document.getElementById("comida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida1").style.display = "none"
     
     document.getElementById("comida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida2").style.display = "none"
     
     document.getElementById("comida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectComida3").style.display = "none"
 }
 
 
@@ -51,96 +67,128 @@ function selecionarComida4() {
 // SELECIONANDO BEBIDA 1
 function selecionarBebida1() {
     document.getElementById("bebida1").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectBebida1").style.display = "block"
     
 // DESELECIONANDO BEBIDA 2 3 4
     document.getElementById("bebida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida2").style.display = "none"
     
     document.getElementById("bebida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida3").style.display = "none"
     
     document.getElementById("bebida4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida4").style.display = "none"
 }
 
 // SELECIONANDO BEBIDA 2
 function selecionarBebida2() {
     document.getElementById("bebida2").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectBebida2").style.display = "block"
     
 // DESELECIONANDO BEBIDA 1 3 4
     document.getElementById("bebida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida1").style.display = "none"
     
     document.getElementById("bebida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida3").style.display = "none"
     
     document.getElementById("bebida4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida4").style.display = "none"
 }
 
 // SELECIONANDO BEBIDA 3
 function selecionarBebida3() {
     document.getElementById("bebida3").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectBebida3").style.display = "block"
     
 // DESELECIONANDO BEBIDA 1 2 4
     document.getElementById("bebida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida1").style.display = "none"
     
     document.getElementById("bebida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida2").style.display = "none"
     
     document.getElementById("bebida4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida4").style.display = "none"
 }
 
 // SELECIONANDO BEBIDA 4
 function selecionarBebida4() {
     document.getElementById("bebida4").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectBebida4").style.display = "block"
     
 // DESELECIONANDO BEBIDA 1 2 3 
     document.getElementById("bebida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida1").style.display = "none"
     
     document.getElementById("bebida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida2").style.display = "none"
     
     document.getElementById("bebida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectBebida3").style.display = "none"
 }
 
 
 // SELECIONANDO SOBREMESA 1
 function selecionarSobremesa1() {
     document.getElementById("sobremesa1").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectSobremesa1").style.display = "block";
     
 // DESELECIONANDO SOBREMESA 2 3 4
     document.getElementById("sobremesa2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa2").style.display = "none";
     
     document.getElementById("sobremesa3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa3").style.display = "none";
     
     document.getElementById("sobremesa4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa4").style.display = "none";
 }
 
 // SELECIONANDO SOBREMESA 2
 function selecionarSobremesa2() {
     document.getElementById("sobremesa2").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectSobremesa2").style.display = "block";
     
 // DESELECIONANDO SOBREMESA 1 3 4
     document.getElementById("sobremesa1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa1").style.display = "none";
     
     document.getElementById("sobremesa3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa3").style.display = "none";
     
     document.getElementById("sobremesa4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa4").style.display = "none";
 }
 
 // SELECIONANDO SOBREMESA 3
 function selecionarSobremesa3() {
     document.getElementById("sobremesa3").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectSobremesa3").style.display = "block";
     
 // DESELECIONANDO SOBREMESA 1 2 4
     document.getElementById("sobremesa1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa1").style.display = "none";
     
     document.getElementById("sobremesa2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa2").style.display = "none";
     
     document.getElementById("sobremesa4").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa4").style.display = "none";
 }
 
 // SELECIONANDO SOBREMESA 4
 function selecionarSobremesa4() {
     document.getElementById("sobremesa4").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("selectSobremesa4").style.display = "block";
     
 // DESELECIONANDO SOBREMESA 1 2 3
     document.getElementById("sobremesa1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa1").style.display = "none";
     
     document.getElementById("sobremesa2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa2").style.display = "none";
     
     document.getElementById("sobremesa3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("selectSobremesa3").style.display = "none";
 }
